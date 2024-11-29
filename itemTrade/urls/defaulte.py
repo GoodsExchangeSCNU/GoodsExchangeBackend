20e93530343e93ce25e8bb204a03a85a673b6575
csf123321 <2652212759@qq.com>
2024-11-29 13:43:47 +0000

    修改文档recommand为recommend

--- itemTrade/urls/defaulte.py
+++ itemTrade/urls/defaulte.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ..views import defaultView
 
 urlpatterns = [
-    path('recommand',defaultView.RecommandView.as_view()),
+    path('recommend',defaultView.RecommandView.as_view()),
     path('search',defaultView.SearchView.as_view())
 ]
